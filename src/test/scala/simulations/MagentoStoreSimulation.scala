91d4b4600b0c9cda977b521f28c081a55a3bc28c
VasylQA <testerok66@gmail.com>
2019-10-01 10:04:03 +0300

    Added comments

--- src/test/scala/simulations/MagentoStoreSimulation.scala
+++ src/test/scala/simulations/MagentoStoreSimulation.scala
@@ -15,9 +15,9 @@ class MagentoStoreSimulation extends Simulation {
     .acceptLanguageHeader("en-US,en;q=0.5")
     .check(status is 200)
 
-  private val createUserExec = RegisterNewUserScenario.createUserScenario.inject(atOnceUsers(Config.users), rampUsers(Config.rampUp) during (20 seconds))
-  private val visitCategoryExec = OpenCategoryPageScenario.openCategoryPage.inject(heavisideUsers(10) during (1 minutes))
-  private val addProductExec = AddProductToCartScenario.addProductScenario.inject(atOnceUsers(Config.users))
+  private val createUserExec = RegisterNewUserScenario.createUserScenario.inject(atOnceUsers(Config.users), rampUsers(Config.rampUp) during (20 seconds)) // example of ramp-up
+  private val visitCategoryExec = OpenCategoryPageScenario.openCategoryPage.inject(heavisideUsers(10) during (1 minutes)) // example of smooth adding of users
+  private val addProductExec = AddProductToCartScenario.addProductScenario.inject(atOnceUsers(Config.users)) // example of adding a number of users at once
   private val openCartWithProduct = OpenShoppingCartWithProductScenario.openShoppingCartScenario.inject(atOnceUsers(Config.users))
 
   setUp(createUserExec.protocols(httpProtocol), openCartWithProduct.protocols(httpProtocol)).maxDuration(21 minutes)
