96a7e353553fa1c0d6a5374b3e7644e021e4c8a3
VasylQA <testerok66@gmail.com>
2019-10-01 13:26:08 +0300

    Removed quotes

--- src/test/scala/requests/AddProductToCartRequest.scala
+++ src/test/scala/requests/AddProductToCartRequest.scala
@@ -7,7 +7,7 @@ import io.gatling.http.Predef._
 object AddProductToCartRequest {
   val addProductToCart = group("Add product to Cart") {
       exec(http("Add product to Cart") // A scenario is a chain of requests and pauses
-        .post(app_url + """/checkout/cart/add/uenc/"${uenc}"/product/3449/""")
+        .post(app_url + """/checkout/cart/add/uenc/${uenc}/product/3449/""")
         .formParam("""product""", "3449")
         .formParam("""item""", """3449""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
         .formParam("""form_key""", "${form_key}")
